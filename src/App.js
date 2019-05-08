d03ae7e1bb50bd366c5d1fd0e41d7dd0dcbb47fd
Esteban Amas <estebanamas@gmail.com>
2019-05-08 19:53:30 -0400

    Added GitHub stats and using React Hooks for async data fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Navigation from './components/Navigation';
+import Home from './components/Home';
+import User from './components/User';
 
-class Home extends React.Component {
-  render() {
-    return <h1>HOME SCREEN</h1>
-  }
-}
 
 class About extends React.Component {
   render() {
@@ -15,17 +12,18 @@ class About extends React.Component {
   }
 }
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Navigation />
       <div className="content">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/about" component={About} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/:id" component={User} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/about" component={About} />
+        </Switch>
       </div>
     </div>
   );
 }
-
-export default App;
